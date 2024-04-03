4344c9b51852f93c8d7cff5b00a2200772029b6f
Ray <ray@redment.com>
2024-04-03 12:47:06 -0700

    feature add: export unit conversions & options

--- gm.py
+++ gm.py
@@ -7,20 +7,16 @@ Garmin Macros: Utilities & helper functions for Garmin Connect.
 --
 Usage
 
-  param[0]      param[1]            param[2]
-  --------      --------            --------
-
   Retrieve all activities for given month and export them as a json file
   ----------------------------------------------------------------------
   gm            (YYYY-MM|tod|today|now|cur|current|month)
 
   Combine all activity json files for a given year and convert them to a simplified CSV
   -------------------------------------------------------------------------------------
-  gm            (gencsv|gen|-g)     {year}
+  GM            Generate CSV        Year           Optional: Subdirectory
+  -------------------------------------------------------------------------------------
+  gm            (gencsv|gen|-g)     {year}         {subdirectory}
 
-  Copy gmdash.html to the gen directory of your personal 'Metrics' location
-  -------------------------------------------------------------------------
-  gm            (cpd|copy-dash)     {gen_directory}
 
   Help manual and version
   -----------------------
@@ -126,7 +122,144 @@ logs_dir = f'{os.path.abspath(os.curdir)}/logs/' # can be shared with activity-m
 gen_dir  = f'{os.path.abspath(os.curdir)}/gen/' # can also be shared with activity-metrics
 gen_srv  = f'{gen_dir}services/garmin/'
 
+def process_export_activities_month(arg1):
+  """Retrieve & export activities for month"""
 
+  input_month = arg1
+
+  if input_month in ('now','cur','current','tod','today','month'):
+    input_month = today.strftime('%Y-%m')
+
+  if not re.match(r'^\d{4}-\d{2}$', input_month):
+    sys.exit(f"Sorry, '{input_month}' is not in the YYYY-MM format. Please correct it.")
+
+  im_split      = input_month.split('-')
+  im_year       = im_split[0]
+  im_month      = im_split[1]
+  im_year_int   = int(im_year)
+  im_month_int  = int(im_month)
+  
+  im_wkd_start, im_last_day = calendar.monthrange(im_year_int, im_month_int)
+
+  input_month_start = datetime.datetime(im_year_int, im_month_int, 1).strftime('%Y-%m-%d')
+  input_month_end   = datetime.datetime(im_year_int, im_month_int, im_last_day).strftime('%Y-%m-%d')
+
+  logs_dir_year = f'{logs_dir}{im_year}/'
+  log_json_file = f'{logs_dir_year}garmin-{im_year}-{im_month}.json'
+
+  if not os.path.isdir(logs_dir_year):
+    sys.exit(f'Sorry, the directory {logs_dir_year} does not exist. Please create it first.')
+
+  api = init_api(email, password)
+  acts_this_month = api.get_activities_by_date(input_month_start,input_month_end)
+
+  f = open(log_json_file, 'w')
+  f.write(to_json(acts_this_month))
+  f.close()
+
+  print(f'Activities ({len(acts_this_month)}) for the month ({input_month}) saved to {log_json_file} successfully.')
+
+
+def convert_unit(val, conv):
+  """Unit converter function"""
+
+  if conv == 'meters_to_miles':
+    if val:
+      return round(float(val)/1609.34, 2)
+    else:
+      return 0
+
+  elif conv == 'seconds_to_duration':
+    if val:
+      secs = val
+      hrs  = int(secs // 3600)
+      secs %= 3600
+      mins = int(secs // 60)
+      secs %= 60
+      secs = round(secs, 2)  # round to two decimal places
+      return '%02d:%02d:%05.2f' % (hrs, mins, secs)
+    else:
+      return 0
+
+  return val
+
+
+def process_gencsv_year(arg1, arg2, arg3):
+  """Combine all activity json files for a given year and convert them to a simplified CSV"""
+  global gen_srv
+
+  if arg2:
+    year = arg2
+    ydir = f'{logs_dir}{year}/'
+    gen_srv = f'{gen_srv}{arg3}/' if arg3 else gen_srv
+    gen_csv_file = f'{gen_srv}{year}-garmin-activities.csv'
+
+    if os.path.exists(ydir) and os.path.isdir(ydir):
+      files = os.listdir(ydir)
+      garmin_files = sorted([file for file in files if file.startswith(f'garmin-{year}-')])
+
+      # -- start: dircheck - double check if the services/garmin/ or services/garmin/{custom} dirs exist before continuing
+      if not os.path.exists(gen_srv) and not os.path.isdir(gen_srv):
+        return print(f'Sorry the directory {gen_srv} does not exist. Please create it.')
+      # -- end: dircheck
+
+      if garmin_files:
+        print(f'Combining all ({len(garmin_files)}) json files for year {year}.')
+        combined_data = []
+        for file in garmin_files:
+          with open(f'{ydir}{file}', 'r') as data:
+            json_data = json.load(data)
+            combined_data.append(json_data)
+
+        flattened_data = [item for sublist in combined_data for item in sublist]
+
+        # temp ignore: 'activityType'
+        final_data = preserve_keys(flattened_data, [
+          'activityId','activityName','startTimeLocal','distance','duration',
+          'averageSpeed','maxSpeed','averageHR','maxHR','description',
+        ])
+
+        # convert the default distance unit: meter -> miles (i.e. divide by 1609.34)
+        final_data = [{k: convert_unit(v, 'meters_to_miles') if k == 'distance' else v for k, v in fd.items()} for fd in final_data]
+
+        # convert the default duration unit: seconds -> duration (i.e. hh:mm:ss)
+        final_data = [{k: convert_unit(v, 'seconds_to_duration') if k == 'duration' else v for k, v in fd.items()} for fd in final_data]
+
+        # sort the list by startTimeLocal with recent times at the top
+        final_data = sorted(final_data, key=lambda x: datetime.datetime.strptime(x['startTimeLocal'], '%Y-%m-%d %H:%M:%S'), reverse=True)
+
+        print('- Converting JSON to CSV...')
+
+        columns = list(final_data[0].keys())
+        csv_string = ','.join(columns) + '\n'
+        for row in final_data:
+          csv_string += ','.join(str(escape_for_csv(row[column])) for column in columns) + '\n'
+
+        print('- JSON successfully converted to CSV.')
+
+        f = open(gen_csv_file, 'w')
+        f.write(csv_string)
+        f.close()
+
+        print(f'File {gen_csv_file} successfully saved.')
+
+      else:
+        print(f'Sorry, we could not find any json files for the year {year}.')
+
+    else:
+      print(f'Sorry, the directory {ydir} could not be found.')
+
+  else:
+    return print('Please enter a valid year value.')
+
+"""
+def process_copydash():
+  gmdash = f'{os.path.dirname(os.path.abspath(os.path.realpath(__file__)))}/gm-dash/public/gm-dash.html'
+  cpfile = sys.argv[2] + '/' if len(sys.argv) == 3 else './'
+  cpfile = f"{cpfile.strip('/')}/gm-dash.html"
+  shutil.copyfile(gmdash, cpfile)
+  print(f'Successfully copied: {gmdash} -> {cpfile}')
+"""
 
 def main():
 
@@ -139,99 +272,20 @@ def main():
   elif sys.argv[1] in ('man','help','-h','--help'):
     return print(man.strip())
 
-  elif sys.argv[1] in ('cpd','copy-dash'):
-    gmdash = f'{os.path.dirname(os.path.abspath(os.path.realpath(__file__)))}/gm-dash/public/gm-dash.html'
-    cpfile = sys.argv[2] + '/' if len(sys.argv) == 3 else './'
-    cpfile = f"{cpfile.strip('/')}/gm-dash.html"
-    shutil.copyfile(gmdash, cpfile)
-    return print(f'Successfully copied: {gmdash} -> {cpfile}')
+  # elif sys.argv[1] in ('cpd','copy-dash'):
+  #  process_copydash()
 
   elif sys.argv[1] in ('gencsv','gen','-g'):
-    if len(sys.argv) == 3 and sys.argv[2]:
-      year = sys.argv[2]
-      ydir = f'{logs_dir}{year}/'
-      gen_csv_file = f'{gen_srv}{year}-garmin-activities.csv'
-
-      if os.path.exists(ydir) and os.path.isdir(ydir):
-        files = os.listdir(ydir)
-        garmin_files = sorted([file for file in files if file.startswith(f'garmin-{year}-')])
-
-        if garmin_files:
-          print(f'Combining all ({len(garmin_files)}) json files for year {year}.')
-          combined_data = []
-          for file in garmin_files:
-            with open(f'{ydir}{file}', 'r') as data:
-              json_data = json.load(data)
-              combined_data.append(json_data)
-
-          flattened_data = [item for sublist in combined_data for item in sublist]
-          # temp ignore: 'activityType'
-          final_data = preserve_keys(flattened_data, ['activityId','activityName','startTimeLocal','distance','duration','averageSpeed','maxSpeed','averageHR','maxHR','description'])
-          final_data = sorted(final_data, key=lambda x: datetime.datetime.strptime(x['startTimeLocal'], '%Y-%m-%d %H:%M:%S'), reverse=True)
-
-          print('- Converting JSON to CSV...')
-
-          columns = list(final_data[0].keys())
-          csv_string = ','.join(columns) + '\n'
-          for row in final_data:
-            csv_string += ','.join(str(escape_for_csv(row[column])) for column in columns) + '\n'
-
-          print('- JSON successfully converted to CSV.')
-
-          f = open(gen_csv_file, 'w')
-          f.write(csv_string)
-          f.close()
-
-          print(f'File {gen_csv_file} successfully saved.')
-
-        else:
-          print(f'Sorry, we could not find any json files for the year {year}.')
-
-      else:
-        print(f'Sorry, the directory {ydir} could not be found.')
-
-    else:
-      return print('Please enter a valid year value.')
+    process_gencsv_year(
+      sys.argv[1], 
+      sys.argv[2] if len(sys.argv) > 2 else False, 
+      sys.argv[3] if len(sys.argv) > 3 else False
+    )
 
   else:
-
-    # retrieve & export activities for month
-
-    input_month = sys.argv[1]
-
-    if input_month in ('now','cur','current','tod','today','month'):
-      input_month = today.strftime('%Y-%m')
-
-    if not re.match(r'^\d{4}-\d{2}$', input_month):
-      sys.exit(f"Sorry, '{input_month}' is not in the YYYY-MM format. Please correct it.")
-
-    im_split      = input_month.split('-')
-    im_year       = im_split[0]
-    im_month      = im_split[1]
-    im_year_int   = int(im_year)
-    im_month_int  = int(im_month)
-    
-    im_wkd_start, im_last_day = calendar.monthrange(im_year_int, im_month_int)
-
-    input_month_start = datetime.datetime(im_year_int, im_month_int, 1).strftime('%Y-%m-%d')
-    input_month_end   = datetime.datetime(im_year_int, im_month_int, im_last_day).strftime('%Y-%m-%d')
-
-    logs_dir_year = f'{logs_dir}{im_year}/'
-    log_json_file = f'{logs_dir_year}garmin-{im_year}-{im_month}.json'
-
-    if not os.path.isdir(logs_dir_year):
-      sys.exit(f'Sorry, the directory {logs_dir_year} does not exist. Please create it first.')
-
-    api = init_api(email, password)
-    acts_this_month = api.get_activities_by_date(input_month_start,input_month_end)
-
-    f = open(log_json_file, 'w')
-    f.write(to_json(acts_this_month))
-    f.close()
-
-    print(f'Activities ({len(acts_this_month)}) for the month ({input_month}) saved to {log_json_file} successfully.')
-
+    process_export_activities_month(sys.argv[1])
 
 
 if __name__ == '__main__':
   main()
+
